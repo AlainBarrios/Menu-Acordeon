2b5a0a5b6ad3d2a5df5efbab85833ff42a2f1d64
AlainBarrios <rtcwkillz@gmail.com>
2015-03-14 20:47:15 -0500

    Update menu_acordeon.js

--- menu_acordeon.js
+++ menu_acordeon.js
@@ -5,8 +5,8 @@ function ini(){
 }
 
 function acordeon(){
-	$("#acordeon ul ul").slideUp();	//Oculta todas las segundas listas desordenadas del elemento #acordeon.
+	$("#acordeon ul ul").slideUp("fast");	//Oculta todas las segundas listas desordenadas del elemento #acordeon.
 	if(!$(this).next().is(":visible")){ //Valida si el elemento siguiente a h3 al que se hizo click no esta visible.
-			$(this).next().slideDown(); //Abre los elementos siguientes del h3 al que se hizo click.
+			$(this).next().slideDown("fast"); //Abre los elementos siguientes del h3 al que se hizo click.
 	}
 }
