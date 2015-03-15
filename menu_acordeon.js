694fb01892570c82a2f40e62fd6efe327b234711
AlainBarrios <rtcwkillz@gmail.com>
2015-03-14 21:28:13 -0500

    Update menu_acordeon.js

--- menu_acordeon.js
+++ menu_acordeon.js
@@ -1,12 +1,13 @@
 $(document).on('ready', ini);
- 
+
 function ini(){
-	$("#acordeon h3").on('click', acordeon);
+	$('#acordeon h3').on('click', acordeon);
 }
 
 function acordeon(){
-	$("#acordeon ul ul").slideUp("fast");	//Oculta todas las segundas listas desordenadas del elemento #acordeon.
-	if(!$(this).next().is(":visible")){ //Valida si el elemento siguiente a h3 al que se hizo click no esta visible.
-			$(this).next().slideDown("fast"); //Abre los elementos siguientes del h3 al que se hizo click.
-	}
+	$('#acordeon ul ul').slideUp();
+  		if(!$(this).next().is(':visible')){
+    			$(this).next().slideDown();
+	 	}
 }
+
